0de419f6623b536fc33ae077c23e266b87176b0d
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2019-11-12 21:58:23 -0500

    Add project files.

---
+++ ComamandLineParser/Options/ProgramOptions.cs
@@ -0,0 +1,17 @@
+﻿using MatthiWare.CommandLine.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComamandLineParser.Options
+{
+   public class ProgramOptions
+    {
+        [Required,Name("u","username"),Description("Login user")]
+        public string Username { get; set; }
+        [Required, Name("p", "password"), Description("password for user")]
+        public string Passwrd { get; set; }
+        [Name("v", "verbose"), Description("verbose output"),DefaultValue(true)]
+        public bool Verbose { get; set; }
+    }
+}
